323f34ed6793ae9964ab3af9c3da56e7ba65d52d
AlanAndreUp <cosaspami7@gmail.com>
2024-08-18 23:21:55 -0600

    fixes

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,18 +1,17 @@
 from transformers import MarianMTModel, MarianTokenizer
 
-# Cargar el modelo y el tokenizador guardados
 model = MarianMTModel.from_pretrained("model/")
 tokenizer = MarianTokenizer.from_pretrained("model/")
 
-# Ejemplo de entrada
+
 input_text = "Quiero volar como un pájaro."
 
-# Tokenizar entrada
+
 inputs = tokenizer(input_text, return_tensors="pt")
 
-# Generar salida
+
 outputs = model.generate(**inputs)
 
-# Decodificar salida
+
 translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(f"Texto traducido: {translated_text}")
